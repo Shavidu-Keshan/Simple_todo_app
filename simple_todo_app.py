9d57bfaf4321aafd398637cda565df5748d06a67
Shavidu Keshan <jkskeshan@sat.appsc.sab.ac.lk>
2024-06-01 10:44:19 +0530

    I add root image

---
+++ simple_todo_app.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Todo App")
+root.geometry('400x500')
+root.resizable(False,False)
+root.iconbitmap('image1.ico')
+
+
+root.mainloop()
