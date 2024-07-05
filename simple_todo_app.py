a680939a460bff36bffc6e5ac2fb3c7e1e1f18aa
Shavidu Keshan <shavidu4321@gmail.com>
2024-07-05 13:54:19 +0530

    some changes

--- simple_todo_app.py
+++ simple_todo_app.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 
 root = tk.Tk()
-root.title("Todo App")
+root.title("Todo Application")
 root.geometry('400x500')
 root.resizable(False,False)
 root.iconbitmap('image1.ico')
