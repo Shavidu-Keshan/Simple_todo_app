f50a083168ce949a1800279b89d448ebf760ef4a
Shavidu Keshan <shavidu4321@gmail.com>
2024-07-11 11:40:04 +0530

    name change

--- simple_todo_app.py
+++ simple_todo_app.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 
 root = tk.Tk()
-root.title("Todo Application")
+root.title("Todo App")
 root.geometry('400x500')
 root.resizable(False,False)
 root.iconbitmap('image1.ico')
